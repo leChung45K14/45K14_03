79420fc695dd8a37ac0f1b8d99abdb21c2c18f47
nhuquynh45k14 <85290154+nhuquynh45k14@users.noreply.github.com>
2022-03-22 20:43:17 +0700

    Add Courses page

---
+++ Japanese/App/Controllers/CoursesController.cs
@@ -0,0 +1,117 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using App.Models;
+using App.Services;
+
+namespace App.Controllers;
+
+public class CoursesController : Controller
+{
+  public IDonHangService _donHangService;
+  public IKhoaHocService _khoaHocService;
+  public IFeedbackService _feedbackService;
+  public IVideoService _videoService;
+
+  public CoursesController(IDonHangService donHangService, IKhoaHocService khoaHocService, IFeedbackService feedbackService, IVideoService videoService)
+  {
+    this._khoaHocService = khoaHocService;
+    this._donHangService = donHangService;
+    this._feedbackService = feedbackService;
+    this._videoService = videoService;
+  }
+
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  public IActionResult N1()
+  {
+    int idCourse = 6;
+    DataRow? course = _khoaHocService.GetKhoaHocById(idCourse);
+
+    List<DataRow> feedbacks = _feedbackService.getFeedbacks(idCourse);
+    ViewData["feedbacks"] = feedbacks;
+
+    return View(course);
+  }
+
+  public IActionResult N2()
+  {
+    int idCourse = 5;
+    DataRow? course = _khoaHocService.GetKhoaHocById(idCourse);
+
+    List<DataRow> feedbacks = _feedbackService.getFeedbacks(idCourse);
+    ViewData["feedbacks"] = feedbacks;
+
+    return View(course);
+  }
+
+  public IActionResult N3()
+  {
+    int idCourse = 4;
+    DataRow? course = _khoaHocService.GetKhoaHocById(idCourse);
+
+    List<DataRow> feedbacks = _feedbackService.getFeedbacks(idCourse);
+    ViewData["feedbacks"] = feedbacks;
+
+    return View(course);
+  }
+
+  public IActionResult N4()
+  {
+    int idCourse = 3;
+    DataRow? course = _khoaHocService.GetKhoaHocById(idCourse);
+
+    List<DataRow> feedbacks = _feedbackService.getFeedbacks(idCourse);
+    ViewData["feedbacks"] = feedbacks;
+
+    return View(course);
+  }
+
+  public IActionResult N5()
+  {
+    int idCourse = 2;
+    DataRow? course = _khoaHocService.GetKhoaHocById(idCourse);
+
+    List<DataRow> feedbacks = _feedbackService.getFeedbacks(idCourse);
+    ViewData["feedbacks"] = feedbacks;
+
+    return View(course);
+  }
+
+  public IActionResult Preview(int idCourse, string idYoutube)
+  {
+    ViewData["idYoutube"] = idYoutube;
+
+    DataRow? course = _khoaHocService.GetKhoaHocById(idCourse);
+    List<DataRow> videos = _videoService.getVideos(idCourse);
+
+    ViewData["videos"] = videos;
+
+    return View(course);
+  }
+
+  [HttpPost]
+  public IActionResult Buy(int idCourse)
+  {
+    Console.WriteLine(idCourse);
+    DataRow? course = _khoaHocService.GetKhoaHocById(idCourse);
+    _donHangService.AddNewDonHang(HttpContext.Session.GetString("Email")!, idCourse, course!.Field<Int64>("DonGia"));
+    return Redirect("/Home/Success");
+  }
+
+  [HttpPost]
+  public IActionResult Feedback(IFormCollection form)
+  {
+    _feedbackService.addFeedback(int.Parse(form["idCourse"]), form["name"], form["message"]);
+    return RedirectToAction(form["actionRedirect"]);
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
+}
