b4269490b2e04fe42017015df29f4f3d7bfb1d10
Thùy Chiêu <chieuthuymaijenny@gmail.com>
2022-03-30 15:16:29 +0700

    Add Japanese Project

---
+++ Japanese/OnlineEdu/Front End/course-detail-n2.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineEdu.Front_End
+{
+    public partial class course_detail_n2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Japanese/OnlineEdu/Front End/index-3.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineEdu
+{
+    public partial class index_3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)                          
+        {
+            TextBox emailInput = (TextBox) Page.PreviousPage.FindControl("emailInput");
+            TextBox password = (TextBox) Page.PreviousPage.FindControl("passwordInput");
+            email.InnerText = emailInput.Text;
+            phone.InnerText = password.Text;
+        }
+    }
+}
