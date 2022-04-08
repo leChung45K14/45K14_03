d153105386b847b8f3d2ff04477232c2225120b6
nhuquynh45k14 <85290154+nhuquynh45k14@users.noreply.github.com>
2022-04-08 19:57:09 +0700

    Add Learning By Video Online Feature

---
+++ Japanese/App/Services/VideoService.cs
@@ -0,0 +1,27 @@
+using System.Data;
+using App.Models;
+
+namespace App.Services
+{
+  public class VideoService : IVideoService
+  {
+    public List<DataRow> getVideos(int idCourse)
+    {
+        var query = Video.getTable().AsEnumerable()
+        .Join(
+            KhoaHoc.getTable().AsEnumerable(),
+            video => video.Field<int>("IDKhoaHoc"),
+            course => course.Field<int>("ID"),
+            (video, course) => new {video = video, course = course})
+        .ToList();
+
+        List<DataRow> result = new List<DataRow>();
+        foreach (var item in query)
+        {
+            result.Add(item.video);
+        }
+
+        return result;
+    }
+  }
+}
